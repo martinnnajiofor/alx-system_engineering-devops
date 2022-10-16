59e7c8c29272c1a2aeaed6a3cbfa1f8b74ae74aa
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-10-16 09:35:36 -0700

    0x0B SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,9 @@
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
+file_line {'set PasswordAuthentication to no':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line {'use school as private key auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
